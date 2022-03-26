c31c3f16feeb11fd653df77bce968f4c677dd423
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-03-26 12:00:04 +0100

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,34 @@
+
+fun main() {
+    val borojaPhoneBook = PhoneBook()
+
+    borojaPhoneBook.storeContact(
+        Contact(
+            "Donnarumma",
+            "Gianluigi",
+            "donnagian@gmail.com",
+            "08123445869"
+        )
+    )
+
+    borojaPhoneBook.storeContact(
+        Contact(
+            "Boroja",
+            "Chinex",
+            "borojachinex@gmail.com",
+            "08034753241"
+        )
+    )
+    //To test duplicates, uncomment
+    borojaPhoneBook.storeContact(
+        Contact(
+            "Alex",
+            "Telles",
+            "donnagian@gmail.com",
+            "08034753241"
+        )
+    )
+    println(borojaPhoneBook.getContact("08123445869"))
+    println()
+    println(borojaPhoneBook.contactList)
+}
