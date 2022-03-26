ef43ff85bff616cef404af9752c826f3006e3459
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-03-26 11:59:19 +0100

    initial commit

---
+++ src/PhoneBook.kt
@@ -0,0 +1,20 @@
+class PhoneBook {
+
+    private val _contactList = mutableListOf<Contact>()
+    val contactList: List<Contact>
+        get() = _contactList
+
+    fun getContact(phoneNumber: String):Contact?{
+        return contactList.find { it.phoneNumber == phoneNumber }
+    }
+
+    fun storeContact(contact: Contact){
+        val duplicateCheck = contactList.map {
+            it.phoneNumber
+        }.contains(contact.phoneNumber)
+        if (duplicateCheck) {
+            throw Exception("Phone number ${contact.phoneNumber} already exists")
+        }
+        _contactList.add(contact)
+    }
+}
