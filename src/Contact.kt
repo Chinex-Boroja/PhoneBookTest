9e1d1c9dacaec78b558b3c59a695d97b08ccee33
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-03-27 09:38:17 +0100

    added date format

--- src/Contact.kt
+++ src/Contact.kt
@@ -1,9 +1,11 @@
-import java.time.LocalDateTime
+import java.text.SimpleDateFormat
+import java.util.*
 
+/* A data class to collate all the contact details typical in a phone number */
 data class Contact(
     val firstName: String,
     val lastName: String,
     val email: String,
     val phoneNumber: String,
-    val dateCreated: LocalDateTime = LocalDateTime.now()
+    val dateCreated: String = SimpleDateFormat("dd/MM/yyyy 'at' HH:mm:ss", Locale.getDefault()).format(Date())
 )
