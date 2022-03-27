5425a40a434ae3719f779165394d7d723d183b28
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-03-27 09:41:13 +0100

    added expection for duplicate contact

--- src/PhoneBook.kt
+++ src/PhoneBook.kt
@@ -4,10 +4,14 @@ class PhoneBook {
     val contactList: List<Contact>
         get() = _contactList
 
+    /* The function below is used to get a phoneNumber from the list of Contact */
     fun getContact(phoneNumber: String):Contact?{
         return contactList.find { it.phoneNumber == phoneNumber }
     }
 
+    /* The function below stores one contact at a time in our phone book.
+    The function also checks for duplicate contact or phoneNumber and throws an exception
+    if the phoneNumber already exist */
     fun storeContact(contact: Contact){
         val duplicateCheck = contactList.map {
             it.phoneNumber
